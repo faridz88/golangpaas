fe780f89a3410d80a952213f5840851e7432d42b
Farid Zarazvand <faridzarazvand@gmail.com>
2024-11-19 18:21:51 +0330

    Update main.go

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func main() {
 	// Hello world, the web server
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Testing Golang APP! PPS t2\n")
+		io.WriteString(w, "Testing Golang APP! v20\n")
 	}
 
 	http.HandleFunc("/", helloHandler)
-    log.Println("Listing for requests at http://localhost:8080/hello")
+    log.Println("Listing for v20 requests at http://localhost:8080/hello")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
