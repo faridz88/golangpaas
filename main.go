eea78c34bc577dd909813656fd26c13d5041e483
Farid Zarazvand <faridzarazvand@gmail.com>
2024-12-19 13:50:35 +0330

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	// Hello world, the web server
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Testing Golang APP! PPS t2\n")
+		io.WriteString(w, "Testing Golang APP! PPS from kelaasor\n")
 	}
 
 	http.HandleFunc("/", helloHandler)
